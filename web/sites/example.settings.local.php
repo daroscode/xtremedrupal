e44a6517e3ca54d057eabfd2e08c9c53212c6c06
Daniel Araujo <danerscode@hotmail.com>
2022-05-04 20:23:53 -0300

    Minor changes

--- web/sites/example.settings.local.php
+++ web/sites/example.settings.local.php
@@ -153,13 +153,3 @@ $settings['skip_permissions_hardening'] = TRUE;
  * the language or field module.
  */
 # $settings['config_exclude_modules'] = ['devel', 'stage_file_proxy'];
-$databases['default']['default'] = array (
-  'database' => 'xtremedrupal',
-  'username' => 'root',
-  'password' => '',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
